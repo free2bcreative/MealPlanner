97f7888f62c6a1148bd8642995ad083ceb27fc5a
Mike Free <free2bcreative@gmail.com>
2015-12-24 01:15:36 -0700

    Initial commit. Have simple random working including undo feature

---
+++ app.js
@@ -0,0 +1,106 @@
+angular.module("MealPlannerApp", [])
+	.controller('CalendarCtrl', function(){
+		var calendar = this;
+		calendar.cal = {
+			sunday: {
+				breakfast: "Green Eggs",
+				lunch: null,
+				dinner: null
+			},
+			monday: {
+				breakfast: null,
+				lunch: null,
+				dinner: null
+			},
+			tuesday: {
+				breakfast: null,
+				lunch: "Ham",
+				dinner: null
+			},
+			wednesday: {
+				breakfast: null,
+				lunch: null,
+				dinner: null
+			},
+			thursday: {
+				breakfast: null,
+				lunch: "Christmas Risengrod",
+				dinner: null
+			},
+			friday: {
+				breakfast: null,
+				lunch: null,
+				dinner: null
+			},
+			saturday: {
+				breakfast: null,
+				lunch: null,
+				dinner: null
+			},
+		};
+		calendar.cal.sunday = {}
+	})
+
+	.controller('SimpleRandomCtrl', function(){
+		var simpleRandom = this;
+		simpleRandom.dinnerIdeaList = [];
+		var backupList = [];
+		var mainList = [
+			"Hawaiian Haystacks",
+			"Meatball and Pineapples",
+			"Fajita Quesadillas",
+			"Spaghetti",
+			"White Chili",
+			"Chili with Quinoa and Chia (corn bread)",
+			"Hamburger Pie",
+			"Turkey dinner",
+			"Calzones (pepperoni, pineapple, peppers, or just cheese)",
+			"Tuna Nuna",
+			"Curry (with bamboo)",
+			"Taco Salad",
+			"Apricot Chicken (with baked sweet potatoes)",
+			"Lasagna/Baked Shells",
+			"Eggs and toast (with fruit)",
+			"Grilled Cheese and soup",
+			"Homemade Mac & Cheese",
+			"Fettuccini Alfredo (kiefer)",
+			"Black bean quinoa enchilada casserole"];
+		this.generateList = function(){
+			shuffle(mainList);
+			simpleRandom.dinnerIdeaList = mainList.slice();
+		}
+		this.remove = function(index) {
+			backupList.push(
+				{
+					index: index, 
+					value: simpleRandom.dinnerIdeaList[index]
+				});
+			simpleRandom.dinnerIdeaList.splice(index,1);
+		}
+		this.canUndo = function(){
+			return backupList.length > 0;
+		}
+		this.undo = function(){
+			lastItem = backupList.pop();
+			simpleRandom.dinnerIdeaList.splice(lastItem.index, 0, lastItem.value);
+		}
+	});
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
